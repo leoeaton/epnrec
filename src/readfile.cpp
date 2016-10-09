045fea16289119cf389a6a83a29284512a62e445
Yi Liao <yliao@se.cuhk.edu.hk>
2016-10-09 19:18:00 +0800

    minor update

--- src/readfile.cpp
+++ src/readfile.cpp
@@ -10,12 +10,26 @@ int user_item_matrix()
         printf("cannot open %s to read!\n", user_item_file.c_str());
         return 1;
     }
+    char buff[BUFF_SIZE_LONG];
+    string line;
     uimatrix = new int *[user_size];
     for (int i = 0; i < user_size; i ++)
     {
         uimatrix[i] = new int[item_size];
-        
+        fgets(buff,BUFF_SIZE_LONG,fin);
+        line = buff;
+        strtokenizer strtok(line," ");
+        if(strtok.count_tokens() != item_size)
+        {
+            printf("the %d of the %d is not equal to the item_size\n",i);
+            return 1;
+            
+        }
+        for(int j = 0; j < item_size; j ++)
+        {
+            uimatrix[i][j] = atoi(strtok.token(j).c_str());
+        }
     }
-    
+    fclose(fin);
+    return 0;
 }
-#endif
